b2a3a49ea40b7173d8a513ee5678f44589a66860
Ania <listopad@agh.edu.pl>
2025-02-08 11:30:06 +0100

    Fix ProductTest

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -4,30 +4,40 @@ import java.math.BigDecimal;
 
 public abstract class Product {
     private final String name;
-
     private final BigDecimal price;
-
     private final BigDecimal taxPercent;
 
-    protected Product(String name, BigDecimal price, BigDecimal tax) {
+    protected Product(String name, BigDecimal price, BigDecimal taxPercent) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+
+        if (price == null || price.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+
+        if (taxPercent == null) {
+            throw new IllegalArgumentException("Tax percent cannot be null");
+        }
+
         this.name = name;
         this.price = price;
-        this.taxPercent = tax;
+        this.taxPercent = taxPercent;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.price.multiply(this.taxPercent).add(this.price);
     }
 }
